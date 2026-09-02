76feefb7f6e62036dfadb2dcc82e6e43c9adcd40
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-09-02 09:12:53 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-02, 09:12 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+select name from employee order by name
