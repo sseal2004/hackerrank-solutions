f1f405844ebce86656e62ea0fb061cf1d19a3722
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-09-04 12:33:37 +0530

    ✅ Average Population of Each Continent [mysql] — approach1

---
+++ solutions/mysql/easy/average-population-of-each-continent/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true
+-- Problem     Average Population of Each Continent
+-- Difficulty  Easy
+-- Subdomain   Basic Join
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-04, 12:33 p.m.
+-- ──────────────────────────────────────────────────
+
+select  c2.continent,floor(avg(c1.population)) from city c1  join country c2 on c1.countrycode = c2.code  group by c2.continent ;
